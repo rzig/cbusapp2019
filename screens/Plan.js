c937664b44f849e6cdfcb2d18e222c9444f0d63c
Ryan Ziegler <rzig408@gmail.com>
2019-02-23 14:12:33 -0500

    created basic camera layout

--- screens/Plan.js
+++ screens/Plan.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet, View, TouchableOpacity } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 import Container from '../components/Container';
 import withHeader from '../helpers/withHeader';
+import { colors } from '../styles/base';
 
 class Plan extends Component {
     static screenInfo = {
@@ -9,14 +11,57 @@ class Plan extends Component {
         left: "chevron-left",
         onLeft: (nav) => nav.navigate("Home")
     }
-    
+
     render() {
         return (
-            <Container>
-                <Text>Hello</Text>
+            <Container style={styles.container}>
+                <RNCamera
+                    ref={ref => this.camera = ref}
+                    style={styles.cam}
+                    type={RNCamera.Constants.Type.back}
+                    flashMode={RNCamera.Constants.FlashMode.off}
+                    permissionDialogTitle="Permission to use camera"
+                    permissionDialogMessage="Please say yes"
+                    captureAudio={false}
+                >
+                    <View style={{flex: 0, flexDirection: "row", justifyContent: "center"}}>
+                        <TouchableOpacity onPress={() => this.capture()}>
+                            <Text>Snap?</Text>
+                        </TouchableOpacity>
+                    </View>
+                </RNCamera>
             </Container>
         )
     }
+
+    async capture() {
+        if(this.camera) {
+            const options = {quality: 0.7, base64: true, doNotSave: true}
+            const data = await this.camera.takePictureAsync(options)
+            console.warn(data.base64);
+        }
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "column",
+        backgroundColor: colors.dark
+    },
+    cam: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20,
+    }
+});
 
 export default withHeader(Plan);

88ef39884d9b9f50eca21be47bf39be4d6934288
Ryan Ziegler <rzig408@gmail.com>
2019-02-28 17:50:24 -0500

    created groups modal

--- components/Group.js
+++ components/Group.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, StyleSheet, Text, ImageBackground } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, ImageBackground, Dimensions, TouchableOpacity } from 'react-native';
 import { colors, measures, fonts } from '../styles/base';
 import posed from 'react-native-pose';
 
@@ -8,19 +8,53 @@ const AnimatedBackground = posed(ImageBackground)({
     disabled: {opacity: 0.3}
 });
 
-const Group = ({name, joined, enabled, style}) => {
-    return (
-        <View style={Object.assign({}, styles.view, style)}>
-            <AnimatedBackground
-              source={require("../assets/images/solarplaceholder1.jpg")}
-              style={Object.assign({}, styles.view, {opacity: 0.3})}
-              imageStyle={{borderRadius: measures.borderRadius}}
-              pose={enabled ? "enabled" : "disabled"}
-            >
-                <Text style={styles.text}>{name}</Text>
-            </AnimatedBackground>
-        </View>
-    )
+const {height, width} = Dimensions.get("window")
+
+const ContainerView = posed.View({
+    open: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        height: height,
+        width: width,
+        backgroundColor: colors.light,
+        zIndex: 11111,
+        borderRadius: 0,
+        margin: 0
+    },
+    closed: {
+        width: "100%",
+        height: 150,
+        marginBottom: measures.margin,
+        backgroundColor: colors.light
+    }
+})
+
+class Group extends Component {
+    render() {
+        const {name, joined, enabled, style, open, onClose, onOpen} = this.props;
+        return (
+            <ContainerView style={styles.view} pose="closed">
+                <TouchableOpacity onPress={onOpen}>
+                    <AnimatedBackground
+                    source={require("../assets/images/solarplaceholder1.jpg")}
+                    style={[styles.view, open ? styles.openImage : {}, {opacity: 0.3}]}
+                    imageStyle={open ? styles.openSubImage : styles.subImage}
+                    pose={enabled ? "enabled" : "disabled"}
+                    >
+                        <Text style={styles.text}>{name}</Text>
+                    </AnimatedBackground>
+                </TouchableOpacity>
+
+                <View style={open ? styles.openDetailsView : styles.detailsView}>
+                    <Text>Group info</Text>
+                    <TouchableOpacity onPress={onClose}>
+                        <Text>Click here to return (temporary).</Text>
+                    </TouchableOpacity>
+                </View>
+            </ContainerView>
+        )
+    }
 }
 
 export default Group;
@@ -30,8 +64,18 @@ const styles = StyleSheet.create({
         width: "100%",
         height: 150,
         marginBottom: measures.margin,
-        borderRadius: measures.borderRadius,
         backgroundColor: colors.light
+    },
+    openView: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        height: height,
+        width: width,
+        backgroundColor: colors.light,
+        zIndex: 11111,
+        borderRadius: 0,
+        margin: 0
     },
     text: {
         color: colors.light,
@@ -41,6 +85,24 @@ const styles = StyleSheet.create({
         top: "30%",
         textShadowColor: colors.dark,
         textShadowOffset: {width: 0.1, height: 0.1},
-        textShadowRadius: 10
+        textShadowRadius: 10,
+        margin: 0,
+        padding: 0
+    },
+    openImage: {
+        height: 150
+    },
+    subImage: {
+        borderRadius: measures.borderRadius
+    },
+    openSubImage: {
+        borderRadius: 0
+    },
+    detailsView: {
+        display: "none"
+    },
+    openDetailsView: {
+        display: "flex",
+        flexDirection: "column"
     }
 });
--- screens/Groups.js
+++ screens/Groups.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TextInput, View, Dimensions, ScrollView } from 'react-native';
 import { bodyText } from '../styles/mixins';
 import Container from '../components/Container';
 import { colors, measures } from '../styles/base';
 import Group from '../components/Group';
-import Icon from 'react-native-vector-icons/AntDesign';
 
 class Groups extends Component {
     state = {
@@ -16,7 +15,8 @@ class Groups extends Component {
             {name: "Group 4", joined: false}
         ],
         validCodes: ["asdf"],
-        groupsEnabled: true
+        groupsEnabled: true,
+        modalOpen: -1
     }
 
     handleCodeChg(newVal) {
@@ -29,7 +29,21 @@ class Groups extends Component {
         });
     }
 
+    onListLayout(e) {
+        const {x, y} = e.nativeEvent.layout;
+    }
+
+    toggleModal(i) {
+        if(this.state.modalOpen !== -1) {
+            this.setState({modalOpen: -1});
+        } else {
+            this.setState({modalOpen: i});
+        }
+    }
+
     render() {
+        let open = this.state.modalOpen > -1;
+        const ListContainer = open ? View : ScrollView;
         return (
             <View style={{display: "flex", flexDirection: "column"}}>
                 <Text style={styles.text}>
@@ -43,22 +57,59 @@ class Groups extends Component {
                     style={styles.input}
                     placeholder={"Group Name"}
                 >
-                    
                 </TextInput>
-                <FlatList
-                    data={this.state.groups}
-                    renderItem={item => <Group name={item.item.name} enabled={item.joined} enabled={this.state.groupsEnabled}/>}
-                    keyExtractor={i => i.name}
-                    style={styles.list}
-                    extraData={this.state.groupsEnabled}
-                    scrollEnabled={this.state.groupsEnabled}
-                />
+                <ListContainer style={[styles.list, open ? styles.openList : {}]} onLayout={e => this.onListLayout(e)}>
+                    <Group
+                        name="Hi"
+                        joined={false}
+                        enabled={this.state.groupsEnabled}
+                        open={this.state.modalOpen == 0}
+                        onOpen={() => this.toggleModal(0)}
+                        onClose={() => this.toggleModal(0)}
+                    />
+                    <Group
+                        name="Bye"
+                        joined={false}
+                        enabled={this.state.groupsEnabled}
+                        open={this.state.modalOpen == 1}
+                        onOpen={() => this.toggleModal(1)}
+                        onClose={() => this.toggleModal(1)}
+                    />
+                    <Group
+                        name="Hello"
+                        joined={false}
+                        enabled={this.state.groupsEnabled}
+                        open={this.state.modalOpen == 2}
+                        onOpen={() => this.toggleModal(2)}
+                        onClose={() => this.toggleModal(2)}
+                    />
+                    <Group
+                        name="Goodbye"
+                        joined={false}
+                        enabled={this.state.groupsEnabled}
+                        open={this.state.modalOpen == 3}
+                        onOpen={() => this.toggleModal(3)}
+                        onClose={() => this.toggleModal(3)}
+                    />
+                </ListContainer>
             </View>
+        )
+    }
+
+    renderGroup(group) {
+        return (
+            <Group
+                enabled={this.state.groupsEnabled}
+                name={group.name}
+                joined={false}
+            />
         )
     }
 }
 
 export default Groups;
+
+const {height, width} = Dimensions.get("window")
 
 const styles = StyleSheet.create({
     text: {
@@ -76,6 +127,15 @@ const styles = StyleSheet.create({
     list: {
         marginTop: measures.margin,
         paddingLeft: 2 * measures.margin,
-        paddingRight: 2 * measures.margin
+        paddingRight: 2 * measures.margin,
+        height: 200
+    },
+    openList: {
+        position: "absolute",
+        top: -1 * measures.barHeight - (measures.margin * 2.5),
+        left: -12,
+        zIndex: 1111,
+        height: height,
+        width: width
     }
 })

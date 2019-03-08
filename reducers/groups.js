5e8e8ce9df193f525c5f3e27e86975277df1e6d4
Ryan Ziegler <rzig408@gmail.com>
2019-03-08 16:30:09 -0500

    enabled modal for hidden groups

--- reducers/groups.js
+++ reducers/groups.js
@@ -6,28 +6,36 @@ const initialState = {groups: [
         oneliner: "Solar for Columbus.",
         about: "We are geeks in CBUS that want to advance solar. We buy once a year.",
         image: "https://blog.ring.com/wp-content/uploads/2016/05/shutterstock_161232668.jpg",
-        panel: panels[0]
+        panel: panels[0],
+        code: "abcde",
+        private: true
     },
     {
         name: "Columbus Nerds 1",
         oneliner: "Solar for Columbus1.",
         about: "We are geeks in CBUS that want to advance solar. We buy once a year.",
         image: "https://blog.ring.com/wp-content/uploads/2016/05/shutterstock_161232668.jpg",
-        panel: panels[0]
+        panel: panels[0],
+        code: "fghij",
+        private: false
     },
     {
         name: "Columbus Nerds 2",
         oneliner: "Solar for Columbus2.",
         about: "We are geeks in CBUS that want to advance solar. We buy once a year.",
         image: "https://blog.ring.com/wp-content/uploads/2016/05/shutterstock_161232668.jpg",
-        panel: panels[0]
+        panel: panels[0],
+        code: "klmno",
+        private: false
     },
     {
         name: "Columbus Nerds 3",
         oneliner: "Solar for Columbus3.",
         about: "We are geeks in CBUS that want to advance solar. We buy once a year.",
         image: "https://blog.ring.com/wp-content/uploads/2016/05/shutterstock_161232668.jpg",
-        panel: panels[0]
+        panel: panels[0],
+        code: "pqrst",
+        private: false
     },
 ]};
 
--- screens/Groups.js
+++ screens/Groups.js
@@ -27,15 +27,18 @@ const mapDispatchToProps = (dispatch) => {
 
 class Groups extends Component {
     state = {
-        validCodes: ["asdf"],
+        validCodes: [],
         groupsEnabled: true,
-        open: -1
+        open: ""
     }
+
+    validCodes = []
 
     handleCodeChg(newVal) {
         this.props.setPreference("groupCode", newVal);
-        if(this.state.validCodes.indexOf(newVal) !== -1) {
+        if(this.validCodes.indexOf(newVal) !== -1) {
             this.props.setGraphics("grayGroups", true);
+            this.setState({open: newVal});
         } else {
             this.props.setGraphics("grayGroups", false);
         }
@@ -73,17 +76,39 @@ class Groups extends Component {
     }
 
     renderGroup(g, i) {
-        return <Group name={g.name} enabled={this.props.displayGroups} key={g.name} onOpen={() => this.setState({open: i})}/>
+        // Here, we do something pretty neat. We "lazy-load"
+        // a list of groups that can be entered. Given that the
+        // state w/ groups can update - even when not on this screen,
+        // it is most efficent to build the array of hidden group codes
+        // here. Additionally, it enforces separation of concerns - other
+        // screens do not need access to this list, and this list is not
+        // important to the overall application. Therefore, it should only exist
+        // in this component.
+        //
+        // Also - we *don't* want to use state for this. Everytime we call
+        // setState, it triggers a re-render. So, when we are calling renderGroup
+        // for each group, it will set the state, and trigger a re-render. As a result,
+        // using state to store validCodes will result in an infinite loop.
+        if(!g.private) {
+            return <Group name={g.name} enabled={this.props.displayGroups} key={g.code} onOpen={() => this.setState({open: i})}/>
+        } else {
+            this.validCodes.push(g.code);
+            return <View key={g.code}/>
+        }
     }
 
     renderModal(g, i) {
+        // We render the modal components for every group,
+        // even if the group is private. Then, when the user
+        // enters a hidden code, we can display the modal
+        // for that hidden group.
         return (
             <GroupModal
                 group={g}
-                open={this.state.open == i}
+                open={this.state.open == g.code}
                 onJoin={() => this.onJoin(g)}
                 onClose={() => this.setState({open: -1})}
-                key={g.name}
+                key={g.code}
             />
         )
     }
